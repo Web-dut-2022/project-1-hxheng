ca101274fe5258c595c0cae34a27558ac0d2bdd9
hxheng <95850538+hxheng@users.noreply.github.com>
2022-04-10 12:13:06 +0800

    Add files via upload

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
 
+from django.urls import path
+from django import template
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('wiki/<str:entry_name>/', views.AllPages, name="AllPages"),
+    path('edit/<str:entry_name>/',views.edit,name='edit'),
+    path('Error', views.Error),
+    path("search_item/", views.search_item),
+    path("create_new_page/",views.create_new_page,name="create_new_page"),
+    path("new_entry/",views.new_entry,name="new_entry")
 ]
